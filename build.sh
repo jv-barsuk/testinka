69a36e2c8390b67692c594eba965936ae1119564
valentino <v.dax@helbling.com>
2024-02-09 12:20:48 +0100

    gui updates and logic updates

--- build.sh
+++ build.sh
@@ -1,22 +1,29 @@
 #! /bin/bash
 # build the final yaml
 
-OUT_FILE=out.yml
+
+DATA_DIR=data
+SUITE_DIR=$DATA_DIR/suites/map
+OUT_FILE=$DATA_DIR/data.yml
+OUT_JSON=$DATA_DIR/data.json
+
+rm $OUT_FILE
+rm $OUT_JSON
 
 echo > $OUT_FILE
 echo -e "suites:\n-" >> $OUT_FILE
 while IFS= read -r line; do 
     echo "  $line" >> $OUT_FILE
-done < suites/avidant/suite.yml
+done < $SUITE_DIR/suite.yml
 echo -e "  sections:" >> $OUT_FILE
-for dir in suites/avidant/*; do
+for dir in $SUITE_DIR/*; do
     if [ -d "$dir" ]; then
         echo "    -" >> $OUT_FILE
         while IFS= read -r line; do     
             echo "      $line" >> $OUT_FILE
         done < $dir/section.yml
+        echo "      test_cases:" >> $OUT_FILE
         for tc in $dir/testcase_*; do
-            echo "      test_cases:" >> $OUT_FILE
             while IFS= read -r line; do   
                 echo "          $line" >> $OUT_FILE
             done < $tc
@@ -24,17 +31,17 @@ for dir in suites/avidant/*; do
     fi
 done
 echo "collections:" >> $OUT_FILE
-for f in $(ls collections/*.yml); do
+for f in $(ls $DATA_DIR/collections/*.yml); do
     cat $f >> $OUT_FILE
     echo -e "\n" >> $OUT_FILE
 done
 
 echo "scenarios:" >> $OUT_FILE
-for f in $(ls scenarios/*.yml); do
+for f in $(ls $DATA_DIR/scenarios/*.yml); do
     cat $f >> $OUT_FILE
     echo -e "\n" >> $OUT_FILE
 done
 
-yq eval -o=json out.yml > out.json
+yq eval -o=json $OUT_FILE > $OUT_JSON
 
 #cat out.json | jq '.collections[]|select(.name=="Basic Tests")'
